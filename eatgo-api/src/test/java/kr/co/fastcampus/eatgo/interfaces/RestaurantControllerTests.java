30f3c1f6308183be787eca54b5006498a9f7a540
dohoon <dlh1106@naver.com>
2020-06-04 00:35:08 +0900

    spring boot gradle project 세팅 및 테스트

---
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTests.java
@@ -0,0 +1,86 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.RestaurantService;
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+import kr.co.fastcampus.eatgo.domain.Restaurant;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(RestaurantController.class)
+class RestaurantControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+//    @SpyBean(RestaurantService.class)
+    @MockBean
+//    @Mock
+    private RestaurantService restaurantService;
+
+//    @SpyBean(RestaurantRepositoryImpl.class)
+//    private RestaurantRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
+
+    @Test
+    public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
+        mvc.perform(get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")))
+                .andExpect(content().string(
+                    containsString("\"name\":\"Bob zip\"")));
+
+    }
+
+    @Test
+    public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("kimchi") );
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber food", "Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
+        mvc.perform(get("/restaurants/1004"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":1004")))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Bob zip\"")))
+                .andExpect(content().string(
+                        containsString("kimchi")));
+
+        mvc.perform(get("/restaurants/2020"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        containsString("\"id\":2020")))
+                .andExpect(content().string(
+                        containsString("\"name\":\"Cyber food\"")));
+
+    }
+}
